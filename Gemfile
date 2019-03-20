b967765b5d6a6491231df7788aac4b60cbd48ede
Melvin1Atieno <melvinatieno@gmail.com>
2019-03-20 12:00:06 +0300

    setup devise

--- Gemfile
+++ Gemfile
@@ -9,6 +9,8 @@ gem 'rails', '~> 5.2.2'
 gem 'pg', '>= 0.18', '< 2.0'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
+# Authentication
+gem 'devise', '~> 4.2'
 # Build JSON APIs with ease. Read more: https://github.com/rails/jbuilder
 # gem 'jbuilder', '~> 2.5'
 # Use Redis adapter to run Action Cable in production
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,8 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Default URL option for devise mailer
+  config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # Code is not reloaded between requests.
   config.cache_classes = true
 
