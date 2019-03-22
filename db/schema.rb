caa33db625d5f8d501a8f8d6de4a6a88ccd21db1
tomshy <tomshy@localhost.localdomain>
2019-03-22 13:08:39 +0300

    updated schema with phone indexing

--- db/schema.rb
+++ db/schema.rb
@@ -49,6 +49,7 @@ ActiveRecord::Schema.define(version: 2019_03_20_135338) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["phone_number"], name: "index_users_on_phone_number", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
