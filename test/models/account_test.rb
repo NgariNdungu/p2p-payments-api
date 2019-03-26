d112c5bb9e7a006aab8de0e9c7327dd56e5b7dde
Melvin1Atieno <melvinatieno@gmail.com>
2019-03-26 19:27:37 +0300

    add test cases for the account model

--- test/models/account_test.rb
+++ test/models/account_test.rb
@@ -1,7 +1,17 @@
 require 'test_helper'
 
 class AccountTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test 'should create account for user' do
+    assert_difference('Account.count') do
+      create(:account, :for_user)
+    end
+  end
+
+  test 'should create account for agency' do
+    assert_difference('Account.count') do
+      create(:account, :for_agency)
+    end
+  end
+
+
 end
