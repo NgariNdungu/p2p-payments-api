555966d8ec770f17dcbb33ef84e94f8196f504ca
tomshy <tomshy@localhost.localdomain>
2019-03-27 17:22:27 +0300

    editted migration file

--- db/migrate/20190320100604_create_accounts.rb
+++ db/migrate/20190320100604_create_accounts.rb
@@ -2,8 +2,7 @@ class CreateAccounts < ActiveRecord::Migration[5.2]
   def change
     create_table :accounts do |t|
       t.integer :balance
-      t.references :user, foreign_key: true
-      t.references :agency, foreign_key: true
+      t.references :user, foreign_key: true      
       t.timestamps
     end
   end
