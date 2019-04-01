ffb83c967119c4a2e1ae6a367d1914a3d96f9d52
Ngari Ndung'u <ngari.ndungu@gmail.com>
2019-04-01 13:03:00 +0300

    use sequences for email and phone

--- test/factories/users.rb
+++ test/factories/users.rb
@@ -1,14 +1,14 @@
 FactoryBot.define do
-  factory :user do  	
+  factory :user do
     sequence(:email, 'a') { |c| "user#{c}@domain.com" }
     password { 'notapassword' }
     sequence(:phone_number, '0701234567')
     full_name { 'first user' }
     factory :user_with_invalid_phone do
-    	phone_number {'0736234234'}
+      phone_number { '0736234234' }
     end
     factory :user_with_weak_password do
-    	password {'qwerty'}
+      password { 'qwerty' }
     end
     factory :loaded_user do
       transient do
