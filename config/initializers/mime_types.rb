d51c227028476c60ebbd97beb8b849dadc7298f4
Melvin1Atieno <melvinatieno@gmail.com>
2019-04-02 15:38:12 +0300

    Add mime types

--- config/initializers/mime_types.rb
+++ config/initializers/mime_types.rb
@@ -1,5 +1,10 @@
 # Be sure to restart your server when you modify this file.
 
+api_mime_types = %W(
+    application/vnd.api+json
+    text/x-json
+    application/json
+)
 # Add new mime types for use in respond_to blocks:
 # Mime::Type.register "text/richtext", :rtf
 Mime::Type.register 'application/vnd.api+json', :json, api_mime_types
