2bc1c42f587fddb0b176735fcfb2b764448fceea
Melvin1Atieno <melvinatieno@gmail.com>
2019-04-03 11:27:57 +0300

    fix routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,5 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'logins', registrations: 'users'}
-
-  devise_scope :user do
-    get 'sign_in', to: 'logins#create'
-    get 'sign_out',  to: 'logins#destroy'
-    post 'sign_up', to: 'users#create'
-  
-  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
 
