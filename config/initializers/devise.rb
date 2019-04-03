f16336157f26a21136f53c04149bceebc89ebde4
Melvin1Atieno <melvinatieno@gmail.com>
2019-04-03 12:33:30 +0300

    fix merge conflicts

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -14,6 +14,9 @@ Devise.setup do |config|
   # Configure the parent class to the devise controllers.
   # config.parent_controller = 'DeviseController'
 
+  config.jwt do |jwt|
+    jwt.secret = ENV['DEVISE_JWT_SECRET_KEY']
+  end
   # ==> Mailer Configuration
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'logins', registrations: 'users'}
+  
+  devise_scope :user do
+    get '/login', to: 'logins#new'
+    get 'logout',  to: 'logins#destroy'
+  end
   resources :agencies, only: [:create, :show, :update, :destroy]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
