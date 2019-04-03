bbfa44f490a3270043b82d7981f12ee2b4d1e962
tomshy <tomshy@localhost.localdomain>
2019-04-03 13:58:06 +0300

    added test initial suite for transaktion controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: {sessions: 'logins', registrations: 'users'}
   resources :agencies, only: [:create, :show, :update, :destroy]
+  scope '/transactions/' do
+  	resources :send_money, action: :send_money, controller: 'transaktions'
+  end
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
