d0a847ba83d1fce5052970a1feebd4847c628dae
Melvin1Atieno <melvinatieno@gmail.com>
2019-04-04 10:57:57 +0300

    wrap up logins

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,5 @@
 class ApplicationController < ActionController::API
-  before_action :authenticate_user!
-
+  #before_action :authenticate_user!
 
 end
 
--- app/controllers/logins_controller.rb
+++ app/controllers/logins_controller.rb
@@ -1,10 +1,13 @@
 class LoginsController < Devise::SessionsController 
+  
+
+  #signs in a  user and returns a token
 
   def new
     email = request.headers['email']
     user = User.find_by(email: email) 
-    if user && sign_in(user)
-      @current_user = user
+    if user && user.valid_password?(request.headers['password'])
+      sign_in(:user, user)
       render json: {data: {auth_token: request.env['warden-jwt_auth.token'] }}
     else
       render json: {data: {title: 'unauthorized', details: 'Incorrect username, password or missing token'}}, status: 401
