efd45a31366217611a502779f62fe2a27ad5406a
tomshy <tomnyongesa5@gmail.com>
2019-04-05 12:31:58 +0300

    test mailer

--- app/mailers/statement_mailer.rb
+++ app/mailers/statement_mailer.rb
@@ -1,6 +1,10 @@
 class StatementMailer < ApplicationMailer
-	
+	default from: "from@example.com"
+	# layout "mail"
 	def send_statement(recipient)
-		mail(to: recipient)
+		@recipient=recipient
+		
+		# attachement
+		mail(to: recipient, subject: "Statement")
 	end
 end
--- test/mailers/statement_mailer_test.rb
+++ test/mailers/statement_mailer_test.rb
@@ -2,9 +2,15 @@ require 'test_helper'
 
 class StatementMailerTest < ActionMailer::TestCase
   test "mail sent" do  	
-  	email=StatementMailer.send_statement('from@example.com')
+  	@recipient=create(:user)
+  	@recipient2=create(:user)
+  	# binding.pry
+  	email=StatementMailer.send_statement(@recipient.email)
   	assert_emails 1 do
   		email.deliver_now
   	end
+  	assert_equal(["usera@domain.com"], email.to)
+  	assert_equal(["from@example.com"], email.from)
+  	assert_equal('Statement', email.subject)
   end
 end
