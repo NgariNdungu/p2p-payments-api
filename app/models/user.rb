fb7c810e1e3f948d5449777e868a7e2910d1e6b4
Ngari Ndung'u <ngari.ndungu@gmail.com>
2019-04-18 18:14:19 +0300

    [skip ci] refactor transaction methods

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,22 +4,27 @@ class User < ApplicationRecord
   has_one :agency
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
-  devise :database_authenticatable, :registerable, :recoverable, :rememberable, :validatable,
+  devise :database_authenticatable, :registerable, :recoverable,
+         :rememberable, :validatable,
          :jwt_authenticatable, jwt_revocation_strategy: self
   validates :phone_number, presence: true, uniqueness: true, length: { is: 10 },
                            format: { with: /07[^35678]+[0-9]+/ }
   validates :full_name, presence: true
   after_create :create_account
 
-
-  def send_money(to:, amount:)
-    Account.transfer(from: account, to: to,
-                     amount: amount, trans_type: 'transfer')
+  def send_money(txn_parameters)
+    send_params = txn_parameters.slice(:phone, :amount)
+    Account.transfer(from: account, to: send_params[:phone],
+                     amount: send_params[:amount], trans_type: 'transfer')
   end
 
-  def withdraw(agent:, amount:)
-    Account.transfer(from: account, to: agent,
-                     amount: amount, trans_type: 'withdrawal')
+  def withdraw(txn_parameters)
+    withdraw_params = txn_parameters.slice(:amount, :agent_number)
+    Account.transfer(from: account, to: withdraw_params[:agent_number],
+                     amount: withdraw_params[:amount], trans_type: 'withdrawal')
   end
 
+  def valid_amount?
+    !amount.to_s.match? /[^0-9.]/
+  end
 end
