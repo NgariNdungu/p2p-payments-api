d58c78de532e41865e396814722182d7768b3327
tomshy <tomnyongesa5@gmail.com>
2019-04-24 16:03:05 +0300

    try generate pdf

---
+++ app/controllers/statement_controller.rb
@@ -0,0 +1,8 @@
+class StatementController < ApplicationController		
+	# def generate_pdf
+
+	# 	# statement = render_to_string pdf: "statement", template: "statment.html.erb", encoding: "UTF-8"
+	# 	# pdf = WickedPdf.new.pdf_from_string(statement)
+	# 	# send_data pdf, filename: 'statement.pdf'
+	# end	
+end
