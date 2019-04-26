c44fe48714936ef0f77d03ae14fa3a8a08cbc44e
Melvin1Atieno <melvinatieno@gmail.com>
2019-04-26 15:47:31 +0300

    clean up auth tests

--- test/controllers/authentications_controller_test.rb
+++ test/controllers/authentications_controller_test.rb
@@ -8,29 +8,24 @@ class AuthenticationsControllerTest < ActionDispatch::IntegrationTest
     @unregistered_user = build(:user)
     @invalid_auth = ActionController::HttpAuthentication::Basic.encode_credentials(@unregistered_user.email, @unregistered_user.password)
   end
-  
-  test "Can sigin registered user" do 
-    get login_url, 
-    headers: {"Accept": "application/vnd.api+json", "HTTP_AUTHORIZATION": @auth}
+
+  test 'Can sigin registered user' do
+    get login_url, headers: { 'Accept': 'application/vnd.api+json', 'HTTP_AUTHORIZATION': @auth}
     assert_response 200
   end
-  
-  test "Does not sign in unregistered user" do
-    get login_url, 
-    headers: {"Accept": "application/vnd.api+json", "HTTP_AUTHORIZATION": @invalid_auth }
+
+  test 'Does not sign in unregistered user' do
+    get login_url, headers: { 'Accept': 'application/vnd.api+json', 'HTTP_AUTHORIZATION': @invalid_auth }
     assert_response 401
   end
  
-  test "Can logout user" do
-    get logout_url, 
-    headers: {"Accept": "application/vnd.api+json", "HTTP_AUTHORIZATION": @auth}
+  test 'Can logout user' do
+    get logout_url, headers: { 'Accept': 'application/vnd.api+json', 'HTTP_AUTHORIZATION': @auth}
     assert_response 204
   end
 
-  test "Does not logout user with invalid token" do
-    get logout_url,
-    headers: {"Accept": "application/vnd.api+json", "HTTP_AUTHORIZATION": @invalid_auth }
+  test 'Does not logout user with invalid token' do
+    get logout_url, headers: { 'Accept': 'application/vnd.api+json', 'HTTP_AUTHORIZATION': @invalid_auth }
     assert_response 401
   end
-
 end
